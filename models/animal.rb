7541d8cf9c41fbd68e7dc63af8d302e20930f060
Ryan <ryanfrost2015@gmail.com>
2019-11-11 12:25:09 +0000

    In animal model, .find has been added

--- models/animal.rb
+++ models/animal.rb
@@ -98,4 +98,12 @@ class Animal
     return results.map { |hash| Animal.new( hash) }
   end
 
+  def self.find( id )
+    sql = "SELECT * FROM animals
+    WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values )
+    return Animal.new( results.first)
+  end
+
 end
